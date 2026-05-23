c79370626b12d1e063e007bd1a332425ce1d7599
Fakhri.Noor <noorollahi.fakhri@gmail.com>
2026-05-23 15:39:24 +0330

    implement useCLickOutside hook

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,10 +1,13 @@
 import { XCircleIcon } from "@heroicons/react/24/outline";
+import useOutsideClick from "../hooks/useOutsideClick";
 
 function Modal({ open, children, title, onClose }) {
+  const ref = useOutsideClick(onClose);
+
   return (
     open && (
       <div className="fixed top-0 left-0 h-screen w-full bg-brand-secondary/40 backdrop-blur-xs center-all">
-        <div className="bg-white rounded-xl p-6 w-80 sm:w-110">
+        <div ref={ref} className="bg-white rounded-xl p-6 w-80 sm:w-110">
           <div className="flex justify-between items-center border-b border-border pb-2 mb-5">
             <p className="text-base font-bold">{title}</p>
             <button onClick={onClose} className="cursor-pointer">
