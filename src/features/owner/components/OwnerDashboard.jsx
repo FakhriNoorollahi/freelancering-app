8bc4a07ca72824f41e6335ec630150e8c6fabeb5
Fakhri.Noor <noorollahi.fakhri@gmail.com>
2026-05-29 01:54:51 +0330

    complete owner dashboard

--- src/features/owner/components/OwnerDashboard.jsx
+++ src/features/owner/components/OwnerDashboard.jsx
@@ -1,23 +1,49 @@
+import {
+  CheckBadgeIcon,
+  ComputerDesktopIcon,
+  FolderIcon,
+} from "@heroicons/react/24/outline";
 import Card from "../../../ui/Card";
-import ownerDashboardData from "../constants/ownerDashboradData";
+import { useOwnerProjects } from "../../../hooks/useOwnerProjects";
+import Sppiner from "../../../ui/Sppiner";
+import { toPersianNumbers } from "../../../utils/toPersianNumber";
 
 function OwnerDashboard() {
+  const { allProjects, isPending } = useOwnerProjects();
+  const completedProjects = allProjects?.reduce(
+    (acc, cur) => acc + !!cur.freelancer,
+    0,
+  );
+
+  const proposals = allProjects?.reduce(
+    (acc, cur) => acc + cur.proposals.length,
+    0,
+  );
+
   return (
     <div className="space-y-10">
       <p className="text-xl font-semibold lg:text-2xl lg:font-bold">آمار کلی</p>
-      <div className="grid grid-cols-3 gap-4">
-        {ownerDashboardData.map((item) => {
-          const IconComponent = item.icon;
-          return (
-            <Card
-              key={item.id}
-              label={item.label}
-              IconComponent={IconComponent}
-              cardClasses="col-span-3 md:col-span-2 lg:col-span-1"
-            />
-          );
-        })}
-      </div>
+      {!isPending ? (
+        <div className="grid grid-cols-3 gap-4">
+          <Card
+            label="پروژه ها"
+            IconComponent={FolderIcon}
+            data={toPersianNumbers(allProjects.length)}
+          />
+          <Card
+            label="پروژه های واگذار شده"
+            IconComponent={CheckBadgeIcon}
+            data={toPersianNumbers(completedProjects)}
+          />
+          <Card
+            label="درخواست ها"
+            IconComponent={ComputerDesktopIcon}
+            data={toPersianNumbers(proposals)}
+          />
+        </div>
+      ) : (
+        <Sppiner />
+      )}
     </div>
   );
 }
--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -1,7 +1,7 @@
-function Card({ IconComponent, label, cardClasses }) {
+function Card({ IconComponent, label, data, cardClasses }) {
   return (
     <div
-      className={`border border-border-opacity rounded-xl bg-white ${cardClasses}`}
+      className={`border border-border-opacity rounded-xl bg-white col-span-3 md:col-span-2 lg:col-span-1 ${cardClasses}`}
     >
       <div className="flex items-center gap-4 p-6">
         <div className="flex items-center justify-center text-white p-2 rounded-xl bg-brand-secondary">
@@ -9,7 +9,7 @@ function Card({ IconComponent, label, cardClasses }) {
         </div>
         <div className="flex flex-col">
           <span className="text-sm text-brand-secondary mb-2">{label}</span>
-          <span className="text-font-primary font-bold">3</span>
+          <span className="text-font-primary font-bold">{data}</span>
         </div>
       </div>
     </div>
