60fe510eedb48668903f3bba9b212b24528b84ee
Fakhri.Noor <noorollahi.fakhri@gmail.com>
2026-06-01 18:03:17 +0330

    fix background in authLayout

--- src/features/auth/AuthLayout.jsx
+++ src/features/auth/AuthLayout.jsx
@@ -1,14 +1,15 @@
 function AuthLayout({ children }) {
   return (
-    <div className="bg-cover bg-[url(/public/images/background.jpg)] bg-no-repeat bg-top-left h-screen">
-      <div className="center-all bg-font-primary/60 backdrop-blur-xs h-full p-4 sm:px-0">
+    <div className="relative h-screen overflow-hidden">
+      <div className="absolute inset-0 bg-[url(/public/images/background.jpg)] bg-no-repeat bg-center bg-cover blur-xs scale-110" />
+      <div className="absolute inset-0 bg-font-primary/65" />
+      <div className="relative center-all h-full p-4 sm:px-0">
         <div className="w-auto sm:w-98 bg-background p-6 rounded-lg text-font-primary">
           <img
             src="/public/images/logo.jpg"
             alt="لوگو"
             className="size-18 rounded-full mx-auto mb-3"
           />
-
           {children}
         </div>
       </div>
