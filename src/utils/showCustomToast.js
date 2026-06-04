f42e5631b84bf8f2491110f807eb4e27d7db5cf6
Fakhri.Noor <noorollahi.fakhri@gmail.com>
2026-06-05 00:15:32 +0330

    remove last toast and show new toast

--- src/utils/showCustomToast.js
+++ src/utils/showCustomToast.js
@@ -2,28 +2,42 @@ import CustomToast from "@/ui/CustomToast";
 import React from "react";
 import toast from "react-hot-toast";
 
-const toastOptions = { duration: 3000, position: "top-center" };
+const toastOptions = {
+  duration: 3000,
+  position: "top-center",
+};
+
+let currentToastId = null;
+
+const showToastWithDismiss = (type, message, options = {}) => {
+  if (currentToastId) {
+    toast.dismiss(currentToastId);
+    currentToastId = null;
+
+    setTimeout(() => {
+      const id = toast.custom(
+        (t) => React.createElement(CustomToast, { t, message, type }),
+        { ...toastOptions, ...options },
+      );
+      currentToastId = id;
+    }, 150);
+  } else {
+    const id = toast.custom(
+      (t) => React.createElement(CustomToast, { t, message, type }),
+      { ...toastOptions, ...options },
+    );
+    currentToastId = id;
+  }
+};
 
 export const showCustomToast = {
   success: (message, options = {}) => {
-    toast.custom(
-      (t) => React.createElement(CustomToast, { t, message, type: "success" }),
-      { ...toastOptions, ...options },
-    );
+    showToastWithDismiss("success", message, options);
   },
   error: (message, options = {}) => {
-    toast.custom(
-      (t) => React.createElement(CustomToast, { t, message, type: "error" }),
-      { ...toastOptions, ...options },
-    );
+    showToastWithDismiss("error", message, options);
   },
   info: (message, options = {}) => {
-    toast.custom(
-      (t) => React.createElement(CustomToast, { t, message, type: "info" }),
-      {
-        ...toastOptions,
-        ...options,
-      },
-    );
+    showToastWithDismiss("info", message, options);
   },
 };
